96244f53a858caa639337371c50062139996a97f
brandon <brandon@onlinehome.de>
2021-12-21 19:21:17 +0000

    add extract from http

--- fantasy_football/io/extract_from_http.py
+++ fantasy_football/io/extract_from_http.py
@@ -1,12 +1,10 @@
-from _pytest.python_api import raises
 import requests
 import logging
 
-def get_html_doc(url:str) -> requests.Response:
-    
+
+def get_html_doc(url: str) -> requests.Response:
     response = requests.get(url=url)
     logging.info(f"Status Code: {response.status_code} for {url}")
-    
     if response.status_code != 200:
         logging.error(f"Could not connect to {url}")
         raise requests.HTTPError
@@ -15,6 +13,7 @@ def get_html_doc(url:str) -> requests.Response:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+    FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(format=FORMAT, level=logging.INFO)
     URL = "https://www.pro-football-reference.com/"
     get_html_doc(url=URL)
--- tests/test_io/test_extract_from_http.py
+++ tests/test_io/test_extract_from_http.py
@@ -4,40 +4,12 @@ import pytest
 from fantasy_football.io.extract_from_http import get_html_doc
 
 
-# def get_my_ip():
-#     response = requests.get(
-#         'http://ipinfo.io/json'
-#     )
-#     return response.json()['ip']
-
-
-# def test_get_my_ip(monkeypatch):
-#     my_ip = '123.123.123.123'
-
-#     class MockResponse:
-
-#         def __init__(self, json_body):
-#             self.json_body = json_body
-
-#         def json(self):
-#             return self.json_body
-
-#     monkeypatch.setattr(
-#         requests,
-#         'get',
-#         lambda *args, **kwargs: MockResponse({'ip': my_ip})
-#     )
-
-#     assert get_my_ip() == my_ip
-
-
 def test_get_html_doc_success(monkeypatch):
 
     class MockResponse:
 
         def __init__(self):
             self.status_code = 200
-
 
     monkeypatch.setattr(
         requests,
@@ -46,6 +18,7 @@ def test_get_html_doc_success(monkeypatch):
     )
 
     assert get_html_doc(url="http://ipinfo.io/json").status_code == 200
+
 
 def test_get_html_doc_exception(monkeypatch):
 
